b72c88ade1754b040176d71bda918c04f6c8c8c7
aybekjumashev <aybek.djumashev@gmail.com>
2025-03-08 22:19:39 +0500

    commit8

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -85,18 +85,21 @@ WSGI_APPLICATION = 'weather_app.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'qoPNFTNwvHTCyhqUSVLEoKpojdbeFSqX',
+#         'HOST': 'maglev.proxy.rlwy.net',    
+#         'PORT': 12589,  
+#     }
+# }
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'qoPNFTNwvHTCyhqUSVLEoKpojdbeFSqX',
-        'HOST': 'maglev.proxy.rlwy.net',    
-        'PORT': 12589,  
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
